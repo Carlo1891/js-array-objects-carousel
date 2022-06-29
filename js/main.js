d66bdda31296e01315a788e16795ea4ed39d5d6b
Carlo1891 <carlo.18@live.it>
2022-06-29 17:11:23 +0200

    Sviluppo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// Dato un array di oggetti letterali con:
-// url dell’immagine
-// titolo
-// descrizione
 // Creare un carosello come nella foto allegata.
 // Milestone 0:
 // Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
@@ -45,3 +41,11 @@ const images = [
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
     },
 ];
+
+// OK visualizza
+// console.log (images); 
+
+let activeImage = 0;
+
+
+
